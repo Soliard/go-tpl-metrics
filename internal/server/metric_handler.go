bec72ddeefb1b8ffe3e1be8e61a1e39aec45453b
Soliard <Soliarder@gmail.com>
2025-06-04 20:56:52 +0700

    fix: claim metric url parser

--- internal/server/metric_handler.go
+++ internal/server/metric_handler.go
@@ -18,7 +18,7 @@ func UpdateClaimMetric(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	metricName, metricType, metricStrValue, err := parseClaimMetricUrl(req.URL.Path)
+	metricType, metricName, metricStrValue, err := parseClaimMetricUrl(req.URL.Path)
 	if err != nil {
 		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
@@ -31,14 +31,14 @@ func UpdateClaimMetric(res http.ResponseWriter, req *http.Request) {
 
 	switch metricType {
 	case models.Gauge:
-		err := updateGaugeMetric(metricName, metricStrValue, res)
+		err := updateGaugeMetric(metricName, metricStrValue)
 		if err != nil {
 			http.Error(res, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 	case models.Counter:
-		err := updateCounterMetric(metricName, metricStrValue, res)
+		err := updateCounterMetric(metricName, metricStrValue)
 		if err != nil {
 			http.Error(res, err.Error(), http.StatusBadRequest)
 			return
@@ -52,39 +52,50 @@ func UpdateClaimMetric(res http.ResponseWriter, req *http.Request) {
 }
 
 func parseClaimMetricUrl(url string) (metricType string, metricName string, metricValue string, err error) {
-	urlParts := make([]string, 0)
-	for _, v := range strings.Split(url, `/`) {
-		if v != "" {
-			urlParts = append(urlParts, v)
-		}
+	segments := getUrlSegments(url)
+	if len(segments) < 2 {
+		err = fmt.Errorf(`URL must be /update/{metricType}/{metricName}/{metricValue}`)
+		return
+	}
+	if len(segments) > 1 {
+		metricType = segments[1]
+	}
+	if len(segments) > 2 {
+		metricName = segments[2]
+	}
+	if len(segments) > 3 {
+		metricValue = segments[3]
 	}
 
-	if len(urlParts) < 4 {
-		return "", "", "", fmt.Errorf(`URL must be /update/{metricType}/{metricName}/{metricValue}`)
-	}
-
-	return urlParts[1], urlParts[2], urlParts[3], nil
+	return
 }
 
-func updateCounterMetric(name string, valueStr string, res http.ResponseWriter) error {
+func getUrlSegments(url string) []string {
+	segments := make([]string, 0)
+	for _, v := range strings.Split(url, `/`) {
+		if v != "" {
+			segments = append(segments, v)
+		}
+	}
+	return segments
+}
+
+func updateCounterMetric(name string, valueStr string) error {
 	metricValue, err := strconv.ParseInt(valueStr, 10, 64)
 	if err != nil {
 		return fmt.Errorf(`invalid metric value, must be an integer`)
 	}
 	storage.UpdateCounter(name, metricValue)
-	sum, _ := storage.GetCounter(name)
-	fmt.Fprintf(res, "counter metric '%s' updated to %d", name, sum)
 
 	return nil
 }
 
-func updateGaugeMetric(name string, valueStr string, res http.ResponseWriter) error {
+func updateGaugeMetric(name string, valueStr string) error {
 	metricValue, err := strconv.ParseFloat(valueStr, 64)
 	if err != nil {
 		return fmt.Errorf(`invalid metric value, must be a float`)
 	}
 	storage.UpdateGauge(name, metricValue)
-	fmt.Fprintf(res, "gauge metric '%s' updated to %f", name, metricValue)
 
 	return nil
 }
