befad36bf93b4fc1ccc095e2984bcf42bc42dfbe
Soliard <Soliarder@gmail.com>
2025-10-20 20:37:49 +0700

    feat: gracefull server shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Soliard/go-tpl-metrics/internal/config"
 	"github.com/Soliard/go-tpl-metrics/internal/logger"
@@ -27,14 +30,13 @@ var (
 // main инициализирует и запускает HTTP сервер для сбора метрик.
 // Создает конфигурацию, логгер, хранилище и HTTP роутер.
 func main() {
-
 	printBuildInfo()
 
-	log.Print("server starting...")
+	fmt.Println("server starting...")
 	defer os.Stdout.Sync()
 	config, err := config.NewServerConfig()
 	if err != nil {
-		log.Fatalf("FATAL: cannot create config: %v", err)
+		log.Fatalf("cannot create config: %v", err)
 	}
 
 	logger, err := logger.New(config.LogLevel)
@@ -44,19 +46,44 @@ func main() {
 	defer logger.Sync()
 	fmt.Printf("server config: %v", config)
 
-	storage, err := store.New(context.TODO(), config)
+	appCtx, appCancel := context.WithCancel(context.Background())
+	defer appCancel()
+
+	storage, err := store.New(appCtx, config)
 	if err != nil {
 		logger.Fatal("error while creating storage", zap.Error(err))
 	}
-	logger.Sugar().Warnf("storage type: %T", storage)
+	fmt.Println("storage type: ", storage)
 
 	service := server.NewMetricsService(storage, config, logger)
 	metricRouter := server.MetricRouter(service)
 
-	err = http.ListenAndServe(service.ServerHost, metricRouter)
-	if err != nil {
-		logger.Fatal("fatal error while server serving", zap.Error(err))
+	srv := &http.Server{
+		Addr:    service.ServerHost,
+		Handler: metricRouter,
 	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal("fatal error while server serving", zap.Error(err))
+		}
+	}()
+
+	<-sigCh
+	logger.Info("shutdown signal received, stopping server...")
+
+	appCancel()
+
+	shCtx, shCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shCancel()
+	if err := srv.Shutdown(shCtx); err != nil {
+		logger.Error("HTTP server Shutdown", zap.Error(err))
+	}
+
+	fmt.Println("server shutdown gracefully")
 }
 
 func printBuildInfo() {
